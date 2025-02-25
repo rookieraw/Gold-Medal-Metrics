aa4f77cd8550994791a53242b210cffffe61d9e8
rookieraw <qaz830612@gmail.com>
2025-02-25 14:02:31 +0800

    Complete mostWinterWins function

--- sql.js
+++ sql.js
@@ -61,7 +61,13 @@ won the most winter medals, along with the number of medals aliased to 'count'.
 */
 
 const mostWinterWins = country => {
-  return;
+  return `SELECT year, COUNT(*) AS count 
+          FROM GoldMedal 
+          WHERE country = '${country}' 
+          AND season = 'Winter' 
+          GROUP BY year 
+          ORDER BY count DESC
+          LIMIT 1;`;
 };
 
 /*
