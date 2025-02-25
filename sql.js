6ba8bcfcf5462008f70e687cb6adda25a19d2fbe
rookieraw <qaz830612@gmail.com>
2025-02-25 14:50:14 +0800

    Complete numberWomenMedalists function

--- sql.js
+++ sql.js
@@ -142,7 +142,10 @@ Returns a SQL query string that will find the number of female medalists.
 */
 
 const numberWomenMedalists = country => {
-  return;
+  return `SELECT COUNT(DISTINCT name) AS count 
+          FROM GoldMedal 
+          WHERE country = '${country}' 
+          AND gender = 'Women';`;
 };
 
 /*
