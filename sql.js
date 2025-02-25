42a46e5c3f5a507e68c6276a15c7c0e0fbfc3043
rookieraw <qaz830612@gmail.com>
2025-02-25 14:10:03 +0800

    Complete bestYear function

--- sql.js
+++ sql.js
@@ -76,7 +76,12 @@ won the most medals, along with the number of medals aliased to 'count'.
 */
 
 const bestYear = country => {
-  return;
+  return `SELECT year, COUNT(*) AS count
+          FROM GoldMedal 
+          WHERE country = '${country}'
+          GROUP BY year
+          ORDER BY count DESC
+          LIMIT 1;`;
 };
 
 /*
