b5d9785a4046ffc92a39d99d91010cd27e1bb4db
rookieraw <qaz830612@gmail.com>
2025-02-25 13:29:26 +0800

    Complete createCountryTable, createGoldMedalTable function

--- sql.js
+++ sql.js
@@ -6,7 +6,11 @@ Returns a SQL query string that will create the Country table with four columns:
 */
 
 const createCountryTable = () => {
-  return;
+  return `CREATE TABLE Country (
+    name TEXT NOT NULL,
+    code TEXT NOT NULL,
+    gdp INTEGER,
+    population INTEGER);`;
 };
 
 /*
@@ -14,7 +18,18 @@ Returns a SQL query string that will create the GoldMedal table with ten columns
 */
 
 const createGoldMedalTable = () => {
-  return;
+  return `CREATE TABLE GoldMedal (
+    id INTEGER PRIMARY KEY, 
+    year INTEGER NOT NULL,
+    city TEXT NOT NULL,
+    season TEXT NOT NULL,
+    name TEXT NOT NULL,
+    country TEXT NOT NULL,
+    gender TEXT NOT NULL,
+    sport TEXT NOT NULL,
+    discipline TEXT NOT NULL,
+    event TEXT NOT NULL
+  );`;
 };
 
 /*
@@ -22,7 +37,7 @@ Returns a SQL query string that will find the number of gold medals for the give
 */
 
 const goldMedalNumber = country => {
-    return;
+  return;
 };
 
 /*
